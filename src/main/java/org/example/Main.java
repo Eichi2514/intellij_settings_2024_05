dfd58df0325ac37615b76a46e6b5d90be0f78693
Eichi2514 <picon1317@gmail.com>
2024-05-31 09:08:33 +0900

    git

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,13 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello! world");
+        int rows = 5;
+        for (int i = 1; i <= rows; i++) {
+            for (int k = 1; k <= i; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+//    // System.out.println();
     }
 }
