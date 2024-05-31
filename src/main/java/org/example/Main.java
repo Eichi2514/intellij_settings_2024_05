05c4bd6d377f2e5c0ea1902d0bb7b22a7be208c5
Eichi2514 <picon1317@gmail.com>
2024-05-31 16:54:26 +0900

    first commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,25 +1,17 @@
 package org.example;
 
-// 배열 안의 값의 총 합, 평균 출력
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int a = 10;
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String date[] = a.split("[.]");
 
-        int[] arr = new int[3];
-        arr[0] = 10;
-        arr[1] = 20;
-        arr[2] = 30;
+        int c = Integer.parseInt(date[0]);
+        int d = Integer.parseInt(date[1]);
+        int e = Integer.parseInt(date[2]);
 
-        System.out.println(arr[0]);
-        System.out.println(arr[1]);
-        System.out.println(arr[2]);
-
-
-        System.out.println(arr[0] + arr[1] + arr[2]);
-        System.out.println((arr[0] + arr[1] + arr[2])/3);
-
-
-
-        // System.out.println();
+        System.out.printf("%04d.%02d.%02d",c,d,e);
     }
 }
