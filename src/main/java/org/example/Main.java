8595455c8aa09fc2db88869db8f4ffaed416316e
Eichi2514 <picon1317@gmail.com>
2024-05-31 13:18:22 +0900

    commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,25 @@
 package org.example;
 
+// 배열 안의 값의 총 합, 평균 출력
 public class Main {
     public static void main(String[] args) {
-        int rows = 5;
-        for (int i = 1; i <= rows; i++) {
-            for (int k = 1; k <= i; k++) {
-                System.out.print("* ");
-            }
-            System.out.println();
-        }
-//    // System.out.println();
+        int a = 10;
+
+        int[] arr = new int[3];
+        arr[0] = 10;
+        arr[1] = 20;
+        arr[2] = 30;
+
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+
+
+        System.out.println(arr[0] + arr[1] + arr[2]);
+        System.out.println((arr[0] + arr[1] + arr[2])/3);
+
+
+
+        // System.out.println();
     }
 }
